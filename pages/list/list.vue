2baf14966b92b321438798921f2652bbc0041e44
hanjingwei <824219116@qq.com>
2022-09-26 02:16:20 +0800

    search

--- pages/list/list.vue
+++ pages/list/list.vue
@@ -1,6 +1,16 @@
 <template>
-	<view>
-		456
+	<view class="page">
+		<view class="header">
+				<!-- <input class="uni-input" focus placeholder="自动获得焦点" /> -->
+				<!-- <button class="mini-btn" type="primary" size="mini">按钮</button> -->
+				<view slot="default" class="search_box">
+					<text class="iconfont">&#xe6e1</text>
+					<input style="width: 280px;" :value="searchName" class="prompt" placeholder="请输入查询关键字" @input="theBlur"></input>
+				</view>
+		</view>
+	 	<view class="content">
+			123
+		</view>
 	</view>
 </template>
 
@@ -8,15 +18,63 @@
 	export default {
 		data() {
 			return {
-				
+				value: ""
 			}
 		},
 		methods: {
-			
+			theBlur(){}
 		}
 	}
 </script>
 
-<style>
-
+<style lang="scss">
+	@import url("@/static/nav/iconfont.css");
+	.page{
+		display: -webkit-flex; /* Safari */
+		display: flex;
+		flex-direction: column;
+		height: 100%;
+	}
+	.header{
+		display: flex;
+		justify-content: center;
+		/* background-color: red;s */
+	}
+	.content{
+		background-color: #000;
+	}
+	.btn-search{
+		
+	}
+	.search_box {
+			width: 400upx;
+			height: 64upx;
+			background-color: #f5f5f5;
+			border-radius: 32upx;
+			display: flex;
+			align-items: center;
+			padding: 0upx 40upx;
+			.prompt {
+				font-size: 28upx;
+				color: #cccccc;
+			}
+		}
+		
+		.iconfont  {
+			background-position: center center;
+			background-repeat: no-repeat;
+			background-size: cover;
+			width: 32upx;
+			height: 28upx;
+			margin-right: 20rpx;
+		}
+		
+		.icon_close {
+			background-position: center center;
+			background-repeat: no-repeat;
+			background-size: cover;
+			width: 32upx;
+			height: 28upx;
+			margin-right: 10rpx;
+		}
 </style>
